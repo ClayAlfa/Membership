01bd1066732cb210fb74b6c5f3ec3cefc669f0ed
ClayAlfa <alfarabibravo8@gmail.com>
2025-05-20 08:20:12 +0800

    update 2

--- views/loginFalse.php
+++ views/loginFalse.php
@@ -8,12 +8,12 @@ include_once 'header.php';
             <a name="about"></a><h2><center>Membership system</center></h2>          
             <div class="descText center-block">
                 <hr class="colorgraph">
-                <p><center>Introduction: Full membership system written in PHP,
-                It includes up to date password salting, secure validation, MySQL injection protection and beautiful design.
+                <p><center>Iki yo Halaman Terakhir luurr,
+                    raonok maleh. mwehehehe
                 <br><br>
 
                 <!-- Remove this to remove the GitHub URL link -->
-                <a href="List Github">Check out this code at GitHub</a><br>
+                <a href="https://github.com/belpythons">Check out this code at GitHub</a><br>
 
                 <!-- Back to main page -->  
                 <a href="Profil Guide">Back to main page</a>
